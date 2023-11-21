34444956fd4b015039282f33ded74ac3b2ae2f3b
WoodenScout <leonardotoffalini@gmail.com>
2023-11-21 21:00:53 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Big text
+
+To install all the dependencies run the following command in your terminal:
+```bash
+pip install requrements.txt
+```
---
+++ solver.py
@@ -0,0 +1,82 @@
+def solve(board):
+    find = find_empty(board)
+    if find:
+        row, col = find
+    else:
+        return True # No more empty spaces
+    
+    for i in range(1, 10): # try out all the numbers in the empty square
+        if valid(board, (row, col), i): # if the number is valid in the empty square
+            board[row][col] = i
+            
+            if solve(board): # if the board is solveable after the number is placed
+                return True
+            
+            board[row][col] = 0 # backtrack
+    return False # board is not solveable
+
+def valid(board, pos, num):
+    # Check row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+        
+    # Check column
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+        
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+    
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+        
+    return True
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j) # row, col
+    return None
+
+
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("-----------------------")
+            
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+                
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+
+def main():
+    board = [
+        [7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+    ]
+    print("Sudoku Board:")
+    print_board(board)
+    solve(board)
+    print("\nSolved Sudoku Board:")
+    print_board(board)
+
+if __name__ == "__main__":
+    main()
